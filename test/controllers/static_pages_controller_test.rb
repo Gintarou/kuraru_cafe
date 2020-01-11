998cbcd4a35b33367264fee0f6053dce22b7af22
rikuto <rikuto.10141219@gmail.com>
2020-01-11 10:20:02 +0900

    Updated

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Kuraru Cafe"
+  end
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "#{@base_title} | About"
+  end
+
+  test "should get rental" do
+    get rental_path
+    assert_response :success
+    assert_select "title", "#{@base_title} | Rental"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "#{@base_title} | Contact"
+  end
+
+  test "should get menu" do
+    get menu_path
+    assert_response :success
+    assert_select "title", "#{@base_title} | Menu"
+  end
+
+end
