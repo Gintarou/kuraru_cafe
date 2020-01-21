5163d442141064ab984d61717bac8cf1e44b810a
rikuto <rikuto.10141219@gmail.com>
2020-01-21 19:47:48 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   resources :lunch_menus
   resources :morning_menus
+  resources :users
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/rental', to: 'static_pages#rental'
   get '/contact', to: 'static_pages#contact'
   get '/menus', to: 'menu#index'
   get '/new', to: 'menu#new'
-  get 'users/new'
   get '/login', to:'sessions#new'
+  get '/signup', to:'users#new'
+  post '/signup', to:'users#create'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
 end
